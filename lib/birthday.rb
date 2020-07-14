284a853c00c82b93855751fa7e36e35a65ac6545
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-14 02:41:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,12 +5,18 @@
 # }
 
 def happy_birthday(birthday_kids)
+<<<<<<< HEAD
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
+=======
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+>>>>>>> c03535649ce81c686a507d3d7cc8f83e1a94fc29
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
+<<<<<<< HEAD
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid,age|
     if age < 13 
@@ -23,4 +29,7 @@ end
 
 
 
+=======
+      
+>>>>>>> c03535649ce81c686a507d3d7cc8f83e1a94fc29
   
